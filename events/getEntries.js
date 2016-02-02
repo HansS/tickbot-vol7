3f3a1c3ce86bcda4ea0200a877e38cc17761123e
Connor Brathwaite <connor.brathwaite@gmail.com>
2016-02-01 20:15:37 -0500

    feature: get entries convo now acepts today as a date param

--- events/getEntries.js
+++ events/getEntries.js
@@ -1,8 +1,11 @@
 import tr from '../lib/requests'
 
-export default ({userId, projectId , startDate, endDate}) => new Promise((resolve, reject) => {
-  const [today] = new Date().toISOString().split('T'),
-    path = `users/${userId}/entries.json?start_date='${startDate}'&end_date='${endDate}'&billable=true"`
+export default ({userId, projectId, startDate, endDate}) => new Promise((resolve, reject) => {
+  const [today] = new Date().toISOString().split('T')
+  if (startDate === 'today') startDate = today
+  if (endDate === 'today') endDate = today
+
+  const path = `users/${userId}/entries.json?start_date='${startDate}'&end_date='${endDate}'&billable=true"`
 
   tr({method: 'GET', path: path})
     .then(projects => resolve(projects))
